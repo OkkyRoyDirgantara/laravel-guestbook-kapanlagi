c99557f2c215a0a1c9a9667984f33bd60a6a5d44
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2021-12-25 14:33:08 +0700

    Modified UI with Bootstrap 5

--- resources/views/guestbook/index.blade.php
+++ resources/views/guestbook/index.blade.php
@@ -1,17 +1,20 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 <h1>List Guestbook</h1>
 {{--Create--}}
 <a href="{{route('guestbook.store')}}">Create</a>
-<table border="1">
+<table class="table">
+    <thead>
     <tr>
         <th>ID</th>
         <th>Name</th>
@@ -20,19 +23,23 @@
         <th>Content</th>
         <th>Address</th>
         <th>Province</th>
+        <th>City</th>
         <th>Created At</th>
         <th>Updated At</th>
         <th>Action</th>
     </tr>
+    </thead>
+    <tbody>
     @foreach($guestbooks as $guestbook)
         <tr>
-            <td>{{ $guestbook->id }}</td>
+            <th>{{ $guestbook->id }}</th>
             <td>{{ $guestbook->first_name. " ".$guestbook->last_name }}</td>
             <td>{{ $guestbook->email }}</td>
             <td>{{ $guestbook->organization }}</td>
             <td>{{ $guestbook->body }}</td>
             <td>{{ $guestbook->address }}</td>
             <td>{{ $guestbook->province }}</td>
+            <td>{{ $guestbook->city }}</td>
             <td>{{ $guestbook->created_at }}</td>
             <td>{{ $guestbook->updated_at }}</td>
             <td>
@@ -46,10 +53,14 @@
             </td>
         </tr>
     @endforeach
+    </tbody>
 </table>
 
 
 {{ $guestbooks->links() }}
 
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
